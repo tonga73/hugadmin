4576b9e77098a85429d95c4c74065e3d7fbe9bf9
gastoire <buzzcutseason73@gmail.com>
2022-06-09 19:18:33 -0300

    Agregado checkAuth a rutas de Tracings

--- backend/routes/tracingRoutes.js
+++ backend/routes/tracingRoutes.js
@@ -8,12 +8,18 @@ import {
   changeStatus,
 } from "../controllers/tracingController.js";
 
+import checkAuth from "../middleware/checkAuth.js";
+
 const router = express.Router();
 
 router.post("/", addTracing);
 
-router.route("/:id").get(getTracing).put(updateTracing).delete(removeTracing);
+router
+  .route("/:id")
+  .get(checkAuth, getTracing)
+  .put(checkAuth, updateTracing)
+  .delete(checkAuth, removeTracing);
 
-router.post("/status/:id", changeStatus);
+router.post("/status/:id", checkAuth, changeStatus);
 
 export default router;
