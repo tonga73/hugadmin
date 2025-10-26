157c872259ceaccb0d641ef277f8643c8a60b601
Gaston Mitoire <gastonmm@gmail.com>
2025-10-26 04:21:54 -0300

    feat: dashboard stats init

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default async function Home() {
   ]);
 
   return (
-    <div className="grid grid-cols-6">
+    <div className="grid grid-cols-2 2xl:grid-cols-6 gap-1.5">
       <CircularProgress
-        color="#000"
+        color="oklch(56.85% 0.1236 237.07)"
         count={total}
         description="Totalidad de expedientes en el sitema"
         title="Total"
